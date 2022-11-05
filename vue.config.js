1b80e339acd22566ca14150c3041558e0d30168f
Kevin55688 <sfong1029@gmail.com>
2022-11-06 02:00:28 +0800

    re build3

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/shopDemo/'
+  publicPath: '/shopDemo/dist/'
 })
